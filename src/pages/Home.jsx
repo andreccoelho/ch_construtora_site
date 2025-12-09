f1bcee692f5d2c729495e0e7c5c1f4ca38c1accc
mlutegar <mlutegar@gmail.com>
2025-12-09 09:52:50 -0300

    feat: adicionando footer em todas as páginas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import ObrasProjetos from "../components/ObrasProjetos/ObrasProjetos";
 import ConhecaCHConstrutora from "../components/ConhecaCHConstrutora/ConhecaCHConstrutora";
 import NossosProjetos from "../components/NossosProjetos/NossosProjetos";
 import ContatoSessao from "../components/ContatoSessao/ContatoSessao";
-import Footer from "../components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -14,7 +13,6 @@ const Home = () => {
       <ConhecaCHConstrutora />
       <NossosProjetos />
       <ContatoSessao />
-      <Footer />
     </Base>
   );
 };
